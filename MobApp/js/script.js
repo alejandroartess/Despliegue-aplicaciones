9e8b70310da9eec1f66a0a92a7deb7a12e265fde
Alejandro Artes <alarso@hotmail.com>
2018-09-19 09:13:52 +0200

    Modificar php

--- MobApp/js/script.js
+++ MobApp/js/script.js
@@ -12,17 +12,6 @@ Copyright © All rights Reserved
 $(function() {
     "use strict";
 
-    /*-----------------------------------
-     * FIXED  MENU - HEADER
-     *-----------------------------------*/
-    function menuscroll() {
-        var $navmenu = $('.nav-menu');
-        if ($(window).scrollTop() > 50) {
-            $navmenu.addClass('is-scrolling');
-        } else {
-            $navmenu.removeClass("is-scrolling");
-        }
-    }
     menuscroll();
     $(window).on('scroll', function() {
         menuscroll();
